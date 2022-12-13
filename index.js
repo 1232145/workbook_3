796614de21456f97b2b570c2cab997aaff3e520a
Duc Huy <51442563+1232145@users.noreply.github.com>
2022-12-12 22:20:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -10,8 +11,11 @@ const router = require('./router/index')
 
 app.use('/', router)
 
+const file = './dataStatistic.json';
 app.get('/system/statistic', async (req, res) => {
-    
+    const readData = await fs.promises.readFile(file, 'utf8');
+    const data = JSON.parse(readData)
+    res.json(data)
 })
 
 const port = 3000;
